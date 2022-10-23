b77be47c4ceb6baee04e74208dbd6e3774d395e4
sun1217shine <51977778+sun1217shine@users.noreply.github.com>
2022-10-23 11:50:05 +0900

    221023
    new portfolio

--- js/main.js
+++ js/main.js
@@ -1,23 +1,19 @@
 $(document).ready(function(){
+    
+    // header
+    var winHeight = $(window).height();
+    
+    $(window).scroll(function () {
+        var winTop = $(document).scrollTop() + 95;
 
+        if(winTop > winHeight){
+            $('header').slideDown(300);
+        }else{
+            $('header').slideUp(300);
+        }
+    }); 
 
-var widthFunc = function(){
-    $('.profile .skill li').eq(0).find('div>div').stop().delay(500).animate({width : '70%'},1000);
-    $('.profile .skill li').eq(1).find('div>div').stop().delay(500).animate({width : '75%'},1000);
-    $('.profile .skill li').eq(2).find('div>div').stop().delay(500).animate({width : '90%'},1000);
-    $('.profile .skill li').eq(3).find('div>div').stop().delay(500).animate({width : '65%'},1000);
-    $('.profile .skill li').eq(4).find('div>div').stop().delay(500).animate({width : '80%'},1000);
-};//스킬 늘리기
-
-$('.menu').on('click',function(){
-    $('.profileCon').stop().animate({left:''},1000);
-    widthFunc();
-});
-
-$('.profile .close').on('click',function(){
-    $('.profileCon').stop().animate({left:'150%'},1000);
-    $('.profile .skill li').find('div>div').stop().animate({width : ''},1000);
-});
-
-
+    
+    // gallery
+    $('.gallery').swipebox();
 });//op
